8adfdb3c36e5fdb7f854f65eae43f6d11e1829a2
philburrrt <75766943+philburrrt@users.noreply.github.com>
2022-05-03 03:01:16 -0500

    batch request & display

--- openseaPrice.py
+++ openseaPrice.py
@@ -8,26 +8,36 @@ with open('./approvedListings.json', 'r') as f:
 
 def updateOSPrices(approvedListings):
 
+    priceLog = []
+
+    for entry in approvedListings:
+        slug = entry['slug']
+        name = entry['assetName']
+        url = 'https://api.opensea.io/api/v1/collection/'
+
+        response = requests.get(url + slug)
+        data = response.json()
+
+        try:
+            floor_price = data['collection']['stats']['floor_price']
+            entry['osPrice'] = floor_price
+            priceDict = {
+                "name": entry['assetName'], 
+                "price": floor_price
+                }
+            priceLog.append(priceDict)
+            time.sleep(0.255)
+        except:
+            print("Error with " + name + " slug")
+            pass
+    
+    pprint.pprint(priceLog)
+    
+
+
+def main():
     while True:
+        updateOSPrices(approvedListings)
+        time.sleep(15)
 
-        for entry in approvedListings:
-            slug = entry['slug']
-            name = entry['assetName']
-            url = 'https://api.opensea.io/api/v1/collection/'
-
-            response = requests.get(url + slug)
-            data = response.json()
-
-            try:
-                floor_price = data['collection']['stats']['floor_price']
-                entry['osPrice'] = floor_price
-                pprint.pprint({
-                    "name": entry['assetName'], 
-                    "price": floor_price
-                    })
-                time.sleep(2)
-            except:
-                print("Error with " + name + " slug")
-                pass
-
-updateOSPrices(approvedListings)
+main()
